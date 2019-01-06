a5f7c53d5ccdc2260d10265945e73e4cbbc1c272
Gaura <gameider12@gmail.com>
2019-01-06 11:04:49 -0500

    add functions set,get and sovle inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The functions given here set and get the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <- function(y){
+	x <<- y
+	inv <<- NULL
+}
+get <- function() x
+setInverse <- function(invrse) inv <<- invrse
+getInverse	<- function() inv
+list(set = set,get = get,setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return inverse of x 
+## if it exists return from cache
+## if it doesn't compute and return
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inv <- x$getInverse()
+if(!is.null(inv)){
+	print("Getting inverse from memory, please wait...")
+	return(inv)
+}
+mtrx <- x$get()
+inv <- solve(mtrx)
+x$setInverse(inv)
+inv
 }
